6f9828d27037a8e8718d490972c84edf41c944ed
Anastasia Zabela <zabela113@mail.ru>
2019-07-30 14:57:34 +0300

    feat: add basic layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import '../styles/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from '../components/layout';
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <Layout>
+      Hello world!
+    </Layout>
+  )
+}
+
+export default IndexPage;
