b2a148118085bd3a12448417ac4a9d912b003938
Anastasia Zabela <zabela113@mail.ru>
2019-07-31 17:42:58 +0300

    feat: add director of the day and design for it

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import Layout from '../components/layout';
 import Description from '../components/description/description-homepage';
+import DirOfTheDay from '../components/director-of-the-day/dir-of-the-day';
 
 const IndexPage = () => {
   return (
     <Layout>
       <Description />
+      <DirOfTheDay />
     </Layout>
   )
 }
