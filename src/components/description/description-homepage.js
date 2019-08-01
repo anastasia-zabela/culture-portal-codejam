97127b719735b3eece61873729dc15b06c32db10
Anastasia Zabela <zabela113@mail.ru>
2019-08-01 13:33:13 +0300

    style: change photo for description

--- src/components/description/description-homepage.js
+++ src/components/description/description-homepage.js
@@ -4,7 +4,7 @@ const Description = () => {
   return (
     <article className="description container-fluid">
       <h2>Theater is a living art</h2>
-      <img className="description__theater-img" alt="theater" width="500px" src="https://images.unsplash.com/photo-1527853410828-e05f067b7707?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+      <img className="description__theater-img" alt="theater" width="500px" src="https://images.unsplash.com/photo-1563903227677-988f00ecc0fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
       <img className="description__director-img" alt="director" width="350px" src="https://images.unsplash.com/photo-1557759677-791f51707d7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80" />
       <div className="square__1"></div>
       <div className="square__2"></div>
