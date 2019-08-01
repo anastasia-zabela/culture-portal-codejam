b71147423ff6c7439e921e5b13ec6251ca822b57
Artyom <a.vecherov@gmail.com>
2019-08-01 04:13:40 +0300

    add directorName section to template

---
+++ src/templates/director-page.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Layout from "../components/layout"
+import { graphql, Link } from "gatsby"
+import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react'
+import directorPageStyles from './director-page.module.scss'
+
+const DirectorPage = ({data}) => {
+  const { directorName, text, image, json, location, gallery } = data.contentfulTheaterDirector;
+
+  return (
+    <Layout>
+      <h1>{directorName}</h1>
+
+
+      <Link to="/directors">View more theater directors</Link>
+      <Link to="/">Home</Link>
+    </Layout>
+  )
+}
+
+export default DirectorPage
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    contentfulTheaterDirector(slug: { eq: $slug }) {
+      directorName
+      slug
+      text {
+        text
+      }
+      image {
+        file {
+          url
+        }
+      }
+      json {
+        entries {
+          key
+          date
+          content
+        }
+      }
+      location {
+        lon
+        lat
+      }
+      gallery {
+        file {
+          url
+        }
+      }
+    }
+  }
+`;
