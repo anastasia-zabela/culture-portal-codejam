8f3adef6317e3cc23f095403cb8b47678470f8d3
Yuri Kazlaik <guinnesswud1@gmail.com>
2019-08-03 16:41:17 +0300

    fix: correct language at the header director page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,58 @@
 import React from 'react';
 import { Link, injectIntl } from 'gatsby-plugin-intl';
+import { Link as LinkGat } from 'gatsby';
 
 import Switcher from '../../components/switcher';
+import be from '../../locales/be.json';
+import ru from '../../locales/ru.json';
+import en from '../../locales/en.json';
+
+const lang = {
+  be: be,
+  ru: ru,
+  en: en
+}
 
 const Header = ({ path, slug, intl }) => {
+  const headerTitle = path ? lang[`${path.slice(-3, -1)}`].header.title : null;
+  const homeLink = path ? lang[`${path.slice(-3, -1)}`].header.nav.home : null;
+  const directorsLink = path ? lang[`${path.slice(-3, -1)}`].header.nav.list : null;
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-3" style={{ paddingLeft: 0 }}>
-            <h4>{intl.formatMessage({ id: "header.title" })}</h4>
-          </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-3">
+              <>
+                {headerTitle ?
+                  (<h4>{headerTitle}</h4>)
+                  :
+                  (<h4>{intl.formatMessage({ id: "header.title" })}</h4>)
+                }
+              </>
+            </div>
             <div className="col-6">
               <ul className="nav justify-content-center">
                 <li className="nav-item active">
-                  <Link className="nav-link" to="/">{intl.formatMessage({ id: "header.nav.home" })}</Link>
+                  <>
+                    {path ?
+                      (<LinkGat className="nav-link" to={`/${path.slice(-3)}/`}>{homeLink}</LinkGat>)
+                      :
+                      (<Link className="nav-link" to="/">{intl.formatMessage({ id: "header.nav.home" })}</Link>)
+                    }
+                  </>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/directors">{intl.formatMessage({ id: "header.nav.list" })}</Link>
+                  <>
+                    {path ?
+                      (<LinkGat className="nav-link" to={`/${path.slice(-3)}/directors/`}>{directorsLink}</LinkGat>)
+                      :
+                      (<Link className="nav-link" to="/directors">{intl.formatMessage({ id: "header.nav.list" })}</Link>)
+                    }
+                  </>
                 </li>
               </ul>
-              </div>
+            </div>
             <div className="col-3">
               <div className="dropdown">
                 <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -34,7 +65,7 @@ const Header = ({ path, slug, intl }) => {
                 </div>
               </div>
             </div>
-              <Switcher path={path} slug={slug} />
+            <Switcher path={path} slug={slug} />
           </div>
         </div>
       </nav>
