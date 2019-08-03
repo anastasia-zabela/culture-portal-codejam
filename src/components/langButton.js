df5ed13ee5c4afd96b749b305acfda7582a9fbb8
Yuri Kazlaik <guinnesswud1@gmail.com>
2019-08-04 01:19:30 +0300

    fix: add correct color on language buttons at template page

--- src/components/langButton.js
+++ src/components/langButton.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { changeLocale, Link } from "gatsby-plugin-intl";
 
 const LangButton = ({ lang, children, currentLocale, path, slug }) => {
+  if (slug) {
+    console.log('slug', slug.slice(-2))
+  }
+  console.log('lang', lang)
   return (
     <>
       {slug ? (
         <button
           style={{
-            color: currentLocale === lang ? `red` : `black`,
             margin: 3,
             cursor: `pointer`,
           }}
-        > <Link to={`/directors/${slug.slice(0, -3)}-${lang}`} > {children}</Link >
+        > <Link
+          style={{
+            color: slug.slice(-2) === lang ? `red` : `black`,
+          }}
+          to={`/directors/${slug.slice(0, -3)}-${lang}`} > {children}</Link >
         </button >
       ) : (
           <button
