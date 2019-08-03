5f993528652500d3c92186596b4be268d5778045
Anastasia Zabela <zabela113@mail.ru>
2019-08-03 15:01:06 +0300

    feat: add template for header with language-plugin

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Link } from 'gatsby-plugin-intl';
+import { Link, injectIntl } from 'gatsby-plugin-intl';
 
 import Switcher from '../../components/switcher';
 
-const Header = ({ path, slug }) => {
+const Header = ({ path, slug, intl }) => {
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
         <div className="row">
           <div className="col-3">
-            <h4>Theater directors of Belarus</h4>
+            <h4>{intl.formatMessage({ id: "header.title" })}</h4>
           </div>
             <div className="col-6">
               <ul className="nav justify-content-center">
                 <li className="nav-item active">
-                  <Link className="nav-link" to="/">Home</Link>
+                  <Link className="nav-link" to="/">{intl.formatMessage({ id: "header.nav.home" })}</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/directors">List of theater directors</Link>
+                  <Link className="nav-link" to="/directors">{intl.formatMessage({ id: "header.nav.list" })}</Link>
                 </li>
               </ul>
               </div>
@@ -42,4 +42,4 @@ const Header = ({ path, slug }) => {
   )
 }
 
-export default Header;
+export default injectIntl(Header);
