e8653bf46be177fc781ab9f1f1fc2514bcc51137
Yuri Kazlaik <guinnesswud1@gmail.com>
2019-08-04 14:09:35 +0300

    feat: add handler for social media buttons

--- src/components/team/member.js
+++ src/components/team/member.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faTelegramPlane } from '@fortawesome/free-brands-svg-icons'
 
-
 const Member = (props) => {
   const data = props.data;
   const style = {
     backgroundImage: data.photo,
+  }
+  const clickHandler = (siteName, accountName) => {
+    const fullUrl = `${siteName}${accountName}`;
+    window.open(fullUrl, '__blank');
   }
   return (
     <div className="row team__member">
@@ -17,8 +20,12 @@ const Member = (props) => {
         <h6>{data.github}</h6>
         <h5>{data.name}</h5>
         <p>{data.contribution}</p>
-        <button type="button" className="btn btn-primary"><FontAwesomeIcon icon={faGithub} /> Github</button>
-        <button type="button" className="btn btn-primary"><FontAwesomeIcon icon={faTelegramPlane} /> Telegram</button>
+        <button type="button" className="btn btn-primary"
+          onClick={() => { clickHandler('https://github.com/', `${data.github.slice(1)}`) }}
+          ><FontAwesomeIcon icon={faGithub} /> Github</button>
+        <button type="button" className="btn btn-primary"
+          onClick={() => { clickHandler('https://t.me/', `${data.telegram.slice(1)}`) }}
+        ><FontAwesomeIcon icon={faTelegramPlane} /> Telegram</button>
       </div>
     </div>
   )
