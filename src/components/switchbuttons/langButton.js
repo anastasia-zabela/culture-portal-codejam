ffb799cf3c4c96dc65100b8e7ce8d8517a261c20
Anastasia Zabela <zabela113@mail.ru>
2019-08-04 03:09:41 +0300

    fix: minor fix at language buttons

--- src/components/switchbuttons/langButton.js
+++ src/components/switchbuttons/langButton.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { changeLocale, Link } from "gatsby-plugin-intl";
 
-const language = {
-  currentLang: 'en'
-};
-
-const LangButton = ({ slug, lang, children }) => {
+const LangButton = ({ slug, lang, children, currentLocale }) => {
   const style = {
     background: '#3d0d15',
   }
@@ -15,19 +11,15 @@ const LangButton = ({ slug, lang, children }) => {
       {slug ? (
           <Link className="btn btn-secondary" 
             to={`/directors/${slug.slice(0, -3)}-${lang}`}
-            style={language.currentLang === lang ? style : {}}
-            onClick={() => {
-              language.currentLang = lang;
-            }}>
+            style={slug.slice(-2) === lang ? style : {}}>
             {children}
           </Link>
       ) : (
         <button className="btn btn-secondary" type="button" 
           onClick={() => {
             changeLocale(lang);
-            language.currentLang = lang;
           }}
-          style={language.currentLang === lang ? style : {}}>
+          style={currentLocale === lang ? style : {}}>
           {children}
         </button>
         )}
